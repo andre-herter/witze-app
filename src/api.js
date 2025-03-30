500744492888937bce4b1a5001a8b65e47b6c9c1
Andre Herter <andre-herter@web.de>
2025-03-30 21:34:08 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,36 +1,38 @@
+let API_ENDPOINT = "https://witzapi.de/api/joke";
+
 export async function getApi() {
-  let API_ENDPOINT = "https://witzapi.de/api/joke"; // Initialisierung von API_ENDPOINT
+  let apiEndpoint = API_ENDPOINT;
   const selectedValue = getSelectValue();
 
   if (selectedValue === "flachwitze") {
-    API_ENDPOINT += "/?category=flachwitze&language=de";
+    apiEndpoint += "/?category=flachwitze&language=de";
   }
   if (selectedValue === "lehrerwitze") {
-    API_ENDPOINT += "/?category=lehrerwitze&language=de";
+    apiEndpoint += "/?category=lehrerwitze&language=de";
   }
   if (selectedValue === "programmierwitze") {
-    API_ENDPOINT += "/?category=programmierwitze&language=de";
+    apiEndpoint += "/?category=programmierwitze&language=de";
   }
   if (selectedValue === "scherzfragen") {
-    API_ENDPOINT += "/?category=scherzfragen&language=de";
+    apiEndpoint += "/?category=scherzfragen&language=de";
   }
   if (selectedValue === "chuck-norris-witze") {
-    API_ENDPOINT += "/?category=chuck-norris-witze&language=de";
+    apiEndpoint += "/?category=chuck-norris-witze&language=de";
   }
   if (selectedValue === "antiwitze") {
-    API_ENDPOINT += "/?category=antiwitze&language=de";
+    apiEndpoint += "/?category=antiwitze&language=de";
   }
   if (selectedValue === "blondinenwitze") {
-    API_ENDPOINT += "/?category=blondinenwitze&language=de";
+    apiEndpoint += "/?category=blondinenwitze&language=de";
   }
   if (selectedValue === "schulwitze") {
-    API_ENDPOINT += "/?category=schulwitze&language=de";
+    apiEndpoint += "/?category=schulwitze&language=de";
   }
   if (selectedValue === "arztwitze") {
-    API_ENDPOINT += "/?category=arztwitze&language=de";
+    apiEndpoint += "/?category=arztwitze&language=de";
   }
 
-  const response = await fetch(API_ENDPOINT);
+  const response = await fetch(apiEndpoint);
 
   const data = await response.json();
   return data[0].text;

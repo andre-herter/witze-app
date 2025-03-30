f5728efba7672e9962a95edb30bfb4d17c21e0df
Andre Herter <andre-herter@web.de>
2025-03-30 21:06:57 +0200

    Update api.js and main.scss

--- src/api.js
+++ src/api.js
@@ -1,51 +1,39 @@
-const API_ENDPOINT = "https://witzapi.de/api/joke";
-const API_ENDPOINT_FLACHWITZ =
-  "https://witzapi.de/api/joke/?category=flachwitze&language=de";
-const API_ENDPOINT_LEHRERWITZ =
-  "https://witzapi.de/api/joke/?category=lehrerwitze&language=de";
-const API_ENDPOINT_PROGRAMMIERWITZE =
-  "https://witzapi.de/api/joke/?category=programmierwitze&language=de";
-const API_ENDPOINT_SCHERZFRAGEN =
-  "https://witzapi.de/api/joke/?limit=1&category=scherzfragen&language=de";
-const API_ENDPOINT_CHUCKNORIS =
-  "https://witzapi.de/api/joke/?category=chuck-norris-witze&language=de";
-const API_ENDPOINT_ANTIWITZE =
-  "https://witzapi.de/api/joke/?category=antiwitze&language=de";
-const API_ENDPOINT_BLONDINENWITZ =
-  "https://witzapi.de/api/joke/?category=blondinenwitze&language=de";
-const API_ENDPOINT_SCHULWITZ =
-  "https://witzapi.de/api/joke/?category=schulwitze&language=de";
-const API_ENDPOINT_ARZTWITZ =
-  "https://witzapi.de/api/joke/?category=arztwitze&language=de";
-
 export async function getApi() {
+  let API_ENDPOINT = "https://witzapi.de/api/joke"; // Initialisierung von API_ENDPOINT
   const selectedValue = getSelectValue();
-  const apiEndpoints = {
-    flachwitze: API_ENDPOINT_FLACHWITZ,
-    lehrerwitze: API_ENDPOINT_LEHRERWITZ,
-    programmierwitze: API_ENDPOINT_PROGRAMMIERWITZE,
-    scherzfragen: API_ENDPOINT_SCHERZFRAGEN,
-    "chuck-norris-witze": API_ENDPOINT_CHUCKNORIS,
-    antiwitze: API_ENDPOINT_ANTIWITZE,
-    blondinenwitze: API_ENDPOINT_BLONDINENWITZ,
-    schulwitze: API_ENDPOINT_SCHULWITZ,
-    arztwitze: API_ENDPOINT_ARZTWITZ,
-  };
 
-  const apiEndpoint = apiEndpoints[selectedValue] || API_ENDPOINT;
-
-  try {
-    const response = await fetch(apiEndpoint);
-    if (!response.ok) {
-      throw new Error("Netzwerkantwort war nicht okay");
-    }
-    const data = await response.json();
-    return data[0].text;
-  } catch (error) {
-    alert("Fehler beim Abrufen des Witzes");
-    console.error("Es gab ein Problem mit der Fetch-Operation:", error);
-    return "Fehler beim Abrufen des Witzes!";
+  if (selectedValue === "flachwitze") {
+    API_ENDPOINT += "/?category=flachwitze&language=de";
   }
+  if (selectedValue === "lehrerwitze") {
+    API_ENDPOINT += "/?category=lehrerwitze&language=de";
+  }
+  if (selectedValue === "programmierwitze") {
+    API_ENDPOINT += "/?category=programmierwitze&language=de";
+  }
+  if (selectedValue === "scherzfragen") {
+    API_ENDPOINT += "/?category=scherzfragen&language=de";
+  }
+  if (selectedValue === "chuck-norris-witze") {
+    API_ENDPOINT += "/?category=chuck-norris-witze&language=de";
+  }
+  if (selectedValue === "antiwitze") {
+    API_ENDPOINT += "/?category=antiwitze&language=de";
+  }
+  if (selectedValue === "blondinenwitze") {
+    API_ENDPOINT += "/?category=blondinenwitze&language=de";
+  }
+  if (selectedValue === "schulwitze") {
+    API_ENDPOINT += "/?category=schulwitze&language=de";
+  }
+  if (selectedValue === "arztwitze") {
+    API_ENDPOINT += "/?category=arztwitze&language=de";
+  }
+
+  const response = await fetch(API_ENDPOINT);
+
+  const data = await response.json();
+  return data[0].text;
 }
 
 function getSelectValue() {

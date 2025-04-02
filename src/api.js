0bf594d5e821ec5b1e3b9d2422672c80b5a9c63a
Andre Herter <andre-herter@web.de>
2025-04-02 12:37:52 +0200

    Add apiEndpoint variables

--- src/api.js
+++ src/api.js
@@ -1,35 +1,14 @@
 const API_ENDPOINT = "https://witzapi.de/api/joke";
 
 export async function getApi() {
-  let apiEndpoint = API_ENDPOINT;
   const selectedValue = getSelectValue();
 
-  if (selectedValue === "flachwitze") {
-    apiEndpoint += "/?category=flachwitze&language=de";
-  }
-  if (selectedValue === "lehrerwitze") {
-    apiEndpoint += "/?category=lehrerwitze&language=de";
-  }
-  if (selectedValue === "programmierwitze") {
-    apiEndpoint += "/?category=programmierwitze&language=de";
-  }
-  if (selectedValue === "scherzfragen") {
-    apiEndpoint += "/?category=scherzfragen&language=de";
-  }
-  if (selectedValue === "chuck-norris-witze") {
-    apiEndpoint += "/?category=chuck-norris-witze&language=de";
-  }
-  if (selectedValue === "antiwitze") {
-    apiEndpoint += "/?category=antiwitze&language=de";
-  }
-  if (selectedValue === "blondinenwitze") {
-    apiEndpoint += "/?category=blondinenwitze&language=de";
-  }
-  if (selectedValue === "schulwitze") {
-    apiEndpoint += "/?category=schulwitze&language=de";
-  }
-  if (selectedValue === "arztwitze") {
-    apiEndpoint += "/?category=arztwitze&language=de";
+  let apiEndpoint = API_ENDPOINT;
+
+  if (selectedValue === "Alle Kategorien") {
+    apiEndpoint;
+  } else {
+    apiEndpoint += `/?category=${selectedValue}&language=de`;
   }
 
   const response = await fetch(apiEndpoint);
